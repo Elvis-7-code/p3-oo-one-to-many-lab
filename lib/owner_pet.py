aac8a054b5ea0e12e7181653a14ed6e87c9b19af
Elvis <elvis.wachira@student.moringaschool.com>
2025-05-22 16:38:04 +0300

    added changes

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,7 +1,19 @@
 class Pet:
     PET_TYPES =["dog", "cat", "rodent", "bird", "reptile", "exotic"]
     all = []
-    
+
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception(f"Invalid pet_type: {pet_type}")
+        if owner is not  None and not isinstance(owner, Owner):
+            raise Exception ("owner must be an instance of Owner or None")
+        
+        self.name = name
+        self.pet_type = pet_type
+        self.owner= owner
+
+        Pet.all.append(self)
+
 
 class Owner:
     def __init__(self, name):
