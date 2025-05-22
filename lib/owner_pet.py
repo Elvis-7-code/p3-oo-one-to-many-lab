ae85eb0d4e898bb48e4715319c64ee950f8167a0
Elvis <elvis.wachira@student.moringaschool.com>
2025-05-22 15:09:38 +0300

    add_pet-owner

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -7,4 +7,10 @@ class Owner:
 
     def pets(self):
         return[pet for pet in Pet.all if pet.owner == self]
-        
+    
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("add_pet argument must be an instance of Pet")
+        pet.owner =self
+    
+    
